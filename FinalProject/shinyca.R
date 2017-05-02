286337017eaeb23332067b7d1162e318cd247902
EJ Arce <ejarce@reed.edu>
2017-05-02 13:33:10 -0700

    still trying to develop the shiny app

--- FinalProject/shinyca.R
+++ FinalProject/shinyca.R
@@ -8,40 +8,45 @@
 #
 
 library(shiny)
+library(leaflet)
 
-# Define UI for application that draws a histogram
+m <- leaflet(SENshape) %>%
+  addPolylines(lng = SENshape$long, lat = SENshape$lat)
+m
+
+# Define UI for application that draws a map
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("California Legislative Elections"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput(inputId = "Year",
-                     "Election:",
-                     min = 1966,
-                     max = 2016)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+  # Application title
+  titlePanel("California Legislative Elections"),
+  
+  # Sidebar with a slider input for year of election 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(inputId = "year",
+                  label = "Election year",
+                  min = 1966,
+                  max = 2016)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      leafletOutput("CAmap")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  
+  output$CAmap <- renderLeaflet({
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
 }
 
 # Run the application 
